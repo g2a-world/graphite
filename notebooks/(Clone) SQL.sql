e8d55dfbbebd6625c3e6617a1664134a1d8ee1a7
g2a-world <g2a-world>
2023-10-19 19:05:02 +0000

    commit

---
+++ notebooks/(Clone) SQL.sql
@@ -0,0 +1,30 @@
+-- Databricks notebook source
+Select * from student_del_db.del_student_rec
+
+-- COMMAND ----------
+
+select * from student_ups_db.ups_student_rec
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- Select * From delta.`/mnt/formula1dlg2a/student-delta/del_student_rec`
+
+-- COMMAND ----------
+
+-- select * from delta.`/mnt/formula1dlg2a/student-upsert/ups_student_rec`
+
+-- COMMAND ----------
+
+-- drop table student_del_db.del_student_rec
+
+-- COMMAND ----------
+
+-- drop table student_ups_db.ups_student_rec
+
+-- COMMAND ----------
+
+
